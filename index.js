bf5ed2757e13a89591ca4f16e468ca0bb690e05d
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2021-12-17 00:31:30 -0800

    missed a variable for cuboidTwo

--- index.js
+++ index.js
@@ -212,11 +212,16 @@ class CuboidMakerTwo{
     return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
   }
 }
+const cuboidTwo = new CuboidMakerTwo({
+  length: 4,
+  height: 5,
+  width: 5,
+})
 
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
-// console.log(cuboidTwo.volume()); // 100
-// console.log(cuboidTwo.surfaceArea()); // 130
+console.log(cuboidTwo.volume()); // 100
+console.log(cuboidTwo.surfaceArea()); // 130
 
 
 
